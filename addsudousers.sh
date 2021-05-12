855051ac365b5d325b97e1665e927373724bfae3
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-05-12 11:49:37 +0300

    Add root control

--- addsudousers.sh
+++ addsudousers.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# For root control
+if [ "$(id -u)" != 0 ]; then
+  echo "You are not root! This script must be run as root"
+  exit 1
+fi
+
+
 # HOME 
 USER=$(logname) 
 eval cd ~$USER

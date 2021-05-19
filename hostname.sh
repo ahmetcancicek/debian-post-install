3dc443f745796004afa1c3d4b67a535aafbc552e
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-05-19 20:17:31 +0300

    Add bash script for hostname

---
+++ hostname.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# For root control
+if [ "$(id -u)" != 0 ]; then
+  echo "You are not root! This script must be run as root"
+  exit 1
+fi
+
+hostnamectl set-hostname $1
+
+
+cat <<EOL
+---------------------------------------------------------------
+Congratulations, your new hostname is $1
+---------------------------------------------------------------
+EOL
+

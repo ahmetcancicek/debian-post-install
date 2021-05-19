cc06d187cad09864a8e232f9b4de33a77bbc387c
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-05-19 18:54:46 +0300

    Fix Docker installation

--- debian-setup.sh
+++ debian-setup.sh
@@ -216,7 +216,7 @@ for choice in $choices; do
       "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io
+    apt-get install -y docker-ce docker-ce-cli containerd.io
     groupadd docker
     usermod -aG docker $USER
     newgrp docker

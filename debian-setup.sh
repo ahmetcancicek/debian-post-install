b048424244203ee2180cd43785498de46df257cb
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-05-26 17:11:56 +0300

    Replace package for vscode

--- debian-setup.sh
+++ debian-setup.sh
@@ -69,7 +69,7 @@ options=(
   E1 "GIT" off
   E2 "JAVA" off
   E3 "GO" off
-  E4 "Microsoft Visual Studio Code (Snap)" off
+  E4 "Microsoft Visual Studio Code" off
   E5 "IntelliJ IDEA Ultimate (Snap)" off
   E6 "GoLand (Snap)" off
   E7 "Postman (Snap)" off
@@ -197,7 +197,13 @@ for choice in $choices; do
   E4)
     # Install Microsoft Visual Studio Code
     echo "Installing Microsoft Visual Studio Code (Snap)"
-    snap install --classic code
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+    sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    rm -f packages.microsoft.gpg
+    apt install apt-transport-https
+    apt update
+    apt install code # or code-insiders
     ;;
   E5)
     # Install IntelliJ IDEA Ultimate

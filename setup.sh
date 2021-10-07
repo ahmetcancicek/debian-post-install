2e716b5d456b662afb90aaec9a842a4c593da375
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2021-10-07 19:31:45 +0300

    Fix Dropbox installation

--- setup.sh
+++ setup.sh
@@ -203,7 +203,7 @@ for choice in $choices; do
     ;;
 
   F1)
-    wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb
+    wget -O dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb
     dpkg -i dropbox.deb
     ;;
   F2)

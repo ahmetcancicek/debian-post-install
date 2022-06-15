0fc59a2b2be1da79142d1c90dd3ae1eb02cb5325
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2022-06-15 20:38:06 +0300

    Refactored package for Spotify

--- setup.sh
+++ setup.sh
@@ -115,7 +115,9 @@ for choice in $choices; do
     apt -y install chromium
     ;;
   B3)
-    snap install spotify
+    curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
+    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+    apt-get update && sudo apt-get install spotify-client
     ;;
   B4)
     snap install opera

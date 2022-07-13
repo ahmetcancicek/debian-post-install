f9cebc5a44c40706d467fc4fbb7e0b8e628e8878
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2022-07-13 13:01:46 +0300

    Fix the bash script for installiation

--- setup.sh
+++ setup.sh
@@ -299,11 +299,11 @@ for choice in $choices; do
     apt-get update
     apt-get -y install docker-ce docker-ce-cli containerd.io
     docker run hello-world
+    groupadd docker
+    usermod -aG docker $USER
     writeInstallationSuccessfulMessage Docker
 
     writeInstallationMessage docker-compose
-    groupadd docker
-    usermod -aG docker $USER
     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose

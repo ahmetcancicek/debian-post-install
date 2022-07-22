578697e4842a58404ef8d6cfe13e9a64b251ea64
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2022-07-22 23:16:04 +0300

    Fix successful message

--- install-sudo.sh
+++ install-sudo.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# Set Color
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+ENDCOLOR="\e[0m"
+
 # For root control
 if [ "$(id -u)" != 0 ]; then
-  echo "You are not root! This script must be run as root"
+  printf "${RED}"
+  cat <<EOL
+========================================================================
+You are not root! This script must be run as root!
+========================================================================
+EOL
+  printf "${ENDCOLOR}"
   exit 1
 fi
 
+#
 apt -y install sudo
 
 # Get USER name
@@ -16,3 +29,16 @@ cat >> /etc/sudoers << EOF
 # User privilege specification
 $USER ALL=(ALL:ALL) ALL
 EOF
+
+
+printf "${GREEN}"
+cat <<EOL
+========================================================================
+Congratulations, everything you wanted to install is installed!
+========================================================================
+EOL
+printf "${ENDCOLOR}"
+
+cat <<EOL
+
+EOL

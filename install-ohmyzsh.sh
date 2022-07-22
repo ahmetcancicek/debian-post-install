891c9a1b015d8782d8e0c3a3663e132ab626c282
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2022-07-22 23:16:28 +0300

    Fix oh-my-zsh installation

--- install-ohmyzsh.sh
+++ install-ohmyzsh.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+# Set Color
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+ENDCOLOR="\e[0m"
+
 # For root control
 if [ "$(id -u)" != 0 ]; then
-  echo "You are not root! This script must be run as root"
+  printf "${RED}"
+  cat <<EOL
+========================================================================
+You are not root! This script must be run as root!
+========================================================================
+EOL
+  printf "${ENDCOLOR}"
   exit 1
 fi
+
+printf "${BLUE}"
+cat <<EOL
+========================================================================
+Oh My Zsh is installing!
+========================================================================
+EOL
+printf "${ENDCOLOR}"
 
 # Get USER name
 USER=$(logname)
@@ -12,9 +32,22 @@ USER=$(logname)
 # Get HOME folder path
 HOME=/home/$USER
 
-chsh -s $(which zsh)
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 source .zshrc
+
+
+printf "${GREEN}"
+cat <<EOL
+========================================================================
+Congratulations, everything you wanted to install is installed!
+========================================================================
+EOL
+printf "${ENDCOLOR}"
+
+cat <<EOL
+
+EOL

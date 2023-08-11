c2d8f1ea259b363262bfc1dfe3318345f72e9ee0
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2023-08-11 23:08:07 +0300

    Fix installation fonts script

--- install-fonts.sh
+++ install-fonts.sh
@@ -36,63 +36,63 @@ cd /tmp
 curl -o open-sans.zip https://fonts.google.com/download?family=Open%20Sans
 unzip open-sans.zip -d open-sans
 rm -rf /usr/share/fonts/open-sans
-mv open-sans /usr/share/fonts/truetype
+mv open-sans /usr/share/fonts/truetype/open-sans
 
 
 # Roboto
 curl -o roboto.zip https://fonts.google.com/download?family=Roboto
 unzip roboto.zip -d roboto
 rm -rf /usr/share/fonts/roboto
-mv roboto /usr/share/fonts/truetype
+mv roboto /usr/share/fonts/truetype/roboto
 
 
 # Noto Sans
 curl -o noto-sans.zip https://fonts.google.com/download?family=Noto%20Sans
 unzip noto-sans.zip -d noto-sans
 rm -rf /usr/share/fonts/noto-sans
-mv noto-sans /usr/share/fonts/truetype
+mv noto-sans /usr/share/fonts/truetype/noto-sans
 
 
-# Fire Mono
+# Fira Mono
 curl -o fira-mono.zip https://fonts.google.com/download?family=Fira%20Mono
 unzip fira-mono.zip -d fira-mono
 rm -rf /usr/share/fonts/fira-mono
-mv fira-mono /usr/share/fonts/truetype
+mv fira-mono /usr/share/fonts/truetype/fira-mono
 
 
 # Clear Sans
 curl -o clear-sans.zip https://www.fontsquirrel.com/fonts/download/clear-sans
 unzip clear-sans.zip -d clear-sans
 rm -rf /usr/share/fonts/clear-sans
-mv clear-sans /usr/share/fonts/truetype
+mv clear-sans /usr/share/fonts/truetype/clear-sans
 
 
-# Fire Sans
+# Fira Sans
 curl -o fira-sans.zip https://fonts.google.com/download?family=Fira%20Sans
 unzip fira-sans.zip -d fira-sans
 rm -rf /usr/share/fonts/fira_sans
-mv fira-sans /usr/share/fonts/truetype
+mv fira-sans /usr/share/fonts/truetype/fira-sans
 
 
 # Roboto Slab
 curl -o roboto-slab.zip https://fonts.google.com/download?family=Roboto%20Slab
 unzip roboto-slab.zip -d roboto-slab
 rm -rf /usr/share/fonts/roboto-slab
-mv roboto-slab /usr/share/fonts/truetype
+mv roboto-slab /usr/share/fonts/truetype/roboto-slab
 
 
 # Overpass
 curl -o overpass.zip https://fonts.google.com/download?family=Overpass
 unzip overpass.zip -d overpass
 rm -rf /usr/share/fonts/overpass
-mv overpass /usr/share/fonts/truetype
+mv overpass /usr/share/fonts/truetype/overpass
 
 
 # Overpass Mono
 curl -o overpass-mono.zip https://fonts.google.com/download?family=Overpass%20Mono
 unzip overpass-mono.zip -d overpass-mono
 rm -rf /usr/share/fonts/overpass-mono
-mv overpass-mono /usr/share/fonts/truetype
+mv overpass-mono /usr/share/fonts/truetype/overpass-mono
 
 
 # Inter
@@ -100,7 +100,7 @@ rm -rf /usr/share/fonts/Inter-3.19
 wget https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip
 unzip Inter-3.19.zip -d Inter-3.19
 rm -rf /usr/share/fonts/inter-3.19
-mv Inter-3.19 /usr/share/fonts/truetype
+mv Inter-3.19 /usr/share/fonts/truetype/Inter-3.19
 
 
 # Hack v3
@@ -118,7 +118,7 @@ cd -
 rm -rf /usr/share/fonts/ubuntu-font-family /usr/share/fonts/ubuntu-font-family-0.83
 curl -o ubuntu-font-family.zip https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip
 unzip ubuntu-font-family.zip -d ./
-mv ubuntu-font-family-0.83 /usr/share/fonts/truetype
+mv ubuntu-font-family-0.83 /usr/share/fonts/truetype/ubuntu-font-family
 fc-cache -f .
 cd -
 
